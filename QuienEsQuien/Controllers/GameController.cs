0cd6710a1ac34b761757e6279a3a2705b8c76893
Mariano Resio <marianolresio@gmail.com>
2018-11-24 15:16:21 -0300

    24.11

--- QuienEsQuien/Controllers/GameController.cs
+++ QuienEsQuien/Controllers/GameController.cs
@@ -14,11 +14,12 @@ namespace QuienEsQuien.Controllers
         // GET: Game
         public ActionResult Index()
         {
+            Session["Primera"] = true;
             Session["BitcoinsARestar"] = 0;
             ViewBag.Categorias = BD.ListarCategorias();
             return View();
         }
-        public ActionResult Mostrar_personajes(int tCate)
+        public ActionResult Categoria(int tCate)
         {
             if (tCate == -1)
             {
@@ -58,25 +59,35 @@ namespace QuienEsQuien.Controllers
                 ViewBag.Lista = ListaPersonajes;
                 Session["ListaPersonajes"] = ListaPersonajes;
             }
-            ViewBag.Categoria = tCate;
+            Session["Categoría"] = tCate;
+
+            return RedirectToAction("Mostrar_personajes");
+        }
+        public ActionResult Mostrar_personajes()
+        {
+            if ((int)Session["BitcoinsARestar"] != 0 && (bool)Session["Primera"] == false)
+            {
+                Session["BitcoinsARestar"] = (int)Session["BitcoinsARestar"] - 5000;
+            }
             return View();
         }
 
-        public ActionResult Preguntas(int tCate)
+        public ActionResult Preguntas()
         {
-            if (tCate == 0)
+            if ((int)Session["Categoría"] == 0)
             {
                 Session["ListaPreguntas"] = BD.ListarPreguntas();
             }
             else
             {
-                Session["ListaPreguntas"] = BD.ListarPreguntasCate(tCate);
+                Session["ListaPreguntas"] = BD.ListarPreguntasCate((int)Session["Categoría"]);
             }
             return View();
         }
-
-        public ActionResult Respuesta(int Pregunta)
+        [HttpPost]
+        public ActionResult Respuesta(string Pregunta0)
         {
+            int Pregunta = Convert.ToInt32(Pregunta0);
             //BORRO LA PREGUNTA DEL SESSION
             List<Preguntas> ListaPreguntas = (List<Preguntas>)Session["ListaPreguntas"];
             int a = ListaPreguntas.Count();
@@ -106,7 +117,26 @@ namespace QuienEsQuien.Controllers
             {
                 ViewBag.Respuesta = true;
             }
+            Session["Primera"] = false;
             return View();
+        }
+        public ActionResult Arriesgar(int Personaje)
+        {
+            if(Personaje == ((Personajes)Session["PersonajeAzar"]).IdPersonaje)
+            {
+                BD.RestarBitcoins((int)Session["BitcoinsARestar"], (int)Session["NombreNow"]);
+                return View("Fin");
+            }
+            else
+            {
+                Session["BitcoinsARestar"] = (int)Session["BitcoinsARestar"] - 50000;
+                ViewBag.Arriesgar = false;
+                return View("Respuesta");
+            }
+        }
+        public ActionResult Arriesgar_personaje()
+        {
+            return View("Arriesgar_personaje");
         }
     }
 }
